0b5d02f7e2703e4d1ad339ccd2cc64a65cac472f
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-08-18 21:18:29 +0000

    Update code for deploy to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gmon$4_6m(5ixx2j_kc58yvp(z@$j021&4e^+vr%us)^e%gh_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-maireadkelly-codestar-4cts28dmgl7.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-maireadkelly-codestar-4cts28dmgl7.ws.codeinstitute-ide.net',
+'.herokuapp.com']
 
 
 # Application definition
