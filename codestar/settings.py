a39a6caed56c613359716eb8968ec36340a068ae
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-08-20 10:28:49 +0000

    Add Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gmon$4_6m(5ixx2j_kc58yvp(z@$j021&4e^+vr%us)^e%gh_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-maireadkelly-codestar-4cts28dmgl7.ws.codeinstitute-ide.net',
 '.herokuapp.com']
@@ -90,6 +90,11 @@ DATABASES = {
     'default':
     dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
