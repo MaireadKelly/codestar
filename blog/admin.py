8ec634540ccda4ae685b8db22fe30ba55a27dc00
Máiréad O'Shea <mairead.kelly26@gmail.com>
2024-08-27 10:47:09 +0000

    Add home page base template

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Post
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title']
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
